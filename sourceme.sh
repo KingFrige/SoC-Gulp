5d5a44e6a544e4435c1daa76a54fe358ad5f5861
woodford.wu <woodford_wu@163.com>
2020-06-19 10:59:16 +0800

    update sourceme for source anywhere

--- sourceme.sh
+++ sourceme.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# please set $SULP_ROOT
-# export SULP_ROOT=$PWD
-
-if [ ! $SULP_ROOT ]; then
-  export SULP_ROOT=$PWD
-fi
-
-export PATH=$SULP_ROOT/node_modules/.bin/:$PATH
-
-if [ $var ];
+if [[ $0 != $BASH_SOURCE ]]
 then
-  export NODE_PATH=$SULP_ROOT/lib:$SULP_ROOT/node_modules:$NODE_PATH
-else
-  export NODE_PATH=$SULP_ROOT/lib:$SULP_ROOT/node_modules
-fi
+  dirpath=`dirname $BASH_SOURCE`
+  abspath=$(cd $dirpath && echo $PWD/${BASH_SOURCE##*/})
+  export SULP_ROOT=`dirname $abspath`
+  if [ ! $SULP_ROOT ]; then
+    export SULP_ROOT=$PWD
+  fi
 
-alias sulp="gulp -f $SULP_ROOT/gulpfile.js"
+  export PATH=$SULP_ROOT/node_modules/.bin/:$PATH
+
+  if [ $NODE_PATH ];
+  then
+    export NODE_PATH=$SULP_ROOT/lib:$SULP_ROOT/node_modules:$NODE_PATH
+  else
+    export NODE_PATH=$SULP_ROOT/lib:$SULP_ROOT/node_modules
+  fi
+
+  alias sulp="gulp -f $SULP_ROOT/gulpfile.js"
+  echo "====================="
+  echo "sulp environment done"
+  echo "====================="
+fi
